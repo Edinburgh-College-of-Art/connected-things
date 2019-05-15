a616e27b9fa6e0ba307cf941831e5e3b2f1ca7d8
mhamilt <matt.hamilton@ed.ac.uk>
2019-05-15 16:25:19 +0100

    feather huzzah wifi examples

---
+++ feather-huzzah/WiFi/get-mac/get-mac.ino
@@ -0,0 +1,16 @@
+/*
+ * Get MAC Address
+ */
+#include <ESP8266WiFi.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  while(!Serial){}
+
+  Serial.println();
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {}
