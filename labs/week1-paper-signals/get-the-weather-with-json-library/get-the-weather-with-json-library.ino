27076c38a3d3c41d45c278182fb16dd90e9ff817
mhamilt <matt.hamilton@ed.ac.uk>
2020-01-13 18:43:38 +0000

    add simplified REST request examples

---
+++ labs/week1-paper-signals/get-the-weather-with-json-library/get-the-weather-with-json-library.ino
@@ -0,0 +1,61 @@
+/*
+   get the weather from the DarkSkyAPI
+*/
+#include <WiFi101.h>
+#include <Servo.h>
+#include <ArduinoJson.h>
+//----------------------------------------------------------------------------
+String yourApiKey = "";
+String host = "api.darksky.net";
+String url = "/forecast/" + yourApiKey + "/55.94100,-3.20356,1578665400?exclude=minutely,flags,hourly,currently,alerts";
+//----------------------------------------------------------------------------
+WiFiSSLClient client;
+//----------------------------------------------------------------------------
+void setup()
+{
+  //----------------------------------------------------------------------------
+  // connect to API
+  if (client.connect(host.c_str(), 443))
+  {
+    client.println("GET " + url + " HTTP/1.1");
+    client.println("Host: " + host);
+    client.println("Connection: close");
+    client.println();
+  }
+  //----------------------------------------------------------------------------
+  // Read json data
+  while (client.connected())
+  {
+    String line = client.readStringUntil('\n');
+    if (line == "\r")
+      break;
+  }
+  String jsonString = client.readString();
+  //----------------------------------------------------------------------------
+  // Read json data to object
+  DynamicJsonDocument buffer(2048);
+  deserializeJson(buffer, jsonString);
+  JsonObject json = buffer.as<JsonObject>();
+  //----------------------------------------------------------------------------
+  // find out if its raning
+  String weather = json["daily"]["data"][0]["icon"];
+  String rain  = "rain";
+  String sleet = "sleet";
+  String snow  = "snow";
+
+  bool moveServo = (weather == rain ||
+                    weather == sleet ||
+                    weather == snow) ? true : false;
+
+  if (moveServo)
+  {
+    // move Servo function
+  }
+
+}
+
+void loop()
+{
+
+
+}
---
+++ labs/week1-paper-signals/get-the-weather/get-the-weather.ino
@@ -0,0 +1,59 @@
+/*
+   get the weather from the DarkSkyAPI
+*/
+#include <WiFi101.h>
+#include <Servo.h>
+//----------------------------------------------------------------------------
+String yourApiKey = "";
+String host = "api.darksky.net";
+String url = "/forecast/" + yourApiKey + "/55.94100,-3.20356,1578665400?exclude=minutely,flags,hourly,currently,alerts";
+//----------------------------------------------------------------------------
+WiFiSSLClient client;
+//----------------------------------------------------------------------------
+void setup()
+{
+  //----------------------------------------------------------------------------
+  // connect to API
+  if (client.connect(host.c_str(), 443))
+  {
+    client.println("GET " + url + " HTTP/1.1");
+    client.println("Host: " + host);
+    client.println("Connection: close");
+    client.println();
+  }
+  //----------------------------------------------------------------------------
+  // Read json data
+  while (client.connected())
+  {
+    String line = client.readStringUntil('\n');
+    if (line == "\r")
+      break;
+  }
+  String jsonString = client.readString();
+  //----------------------------------------------------------------------------
+  // Read json data to object
+  int i = jsonString.indexOf("icon") + 7; // index of icon, skip 7 characters to start of value
+  int j =  jsonString.indexOf("\"", i);   // get next closing " at the end of the 'icon' value
+  String weather = jsonString.substring(i, j); 
+  //----------------------------------------------------------------------------
+  // find out if its raning 
+  String rain  = "rain";
+  String sleet = "sleet";
+  String snow  = "snow";
+
+  bool moveServo = (weather == rain ||
+                    weather == sleet ||
+                    weather == snow) ? true : false;
+
+  if (moveServo)
+  {
+    // move Servo function
+  }
+
+}
+
+void loop()
+{
+
+
+}
