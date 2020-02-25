06f49d1bf98531229afb3310f66bffa2f1aa53d0
mhamilt <matt.hamilton@ed.ac.uk>
2020-02-25 16:47:31 +0000

    change comments

--- example-code/API_request_template/API_request_template.ino
+++ example-code/API_request_template/API_request_template.ino
@@ -5,9 +5,9 @@
 
    Who wrote it? and when?
 
-   What equipment do I need
+   What equipment do I need?
 
-   What pins are connected to what?  
+   What pins are connected to what?
 
    | Arduino Pin | Device pin |
    | ----------- | ---------- |
@@ -19,13 +19,15 @@
    | GND         |            |
 
   Do you need to add any certificates?
+
+  Do you need to add any Libraries?
 */
 
 //---------------------------------------------------------------------------------------------------------------------------------------
 // Libraries
 #ifdef ARDUINO_SAMD_ZERO
 #pragma message "You are Using the Feather M0"
-#include <WiFi101.h> 
+#include <WiFi101.h>
 #endif
 
 #ifdef ARDUINO_SAMD_NANO_33_IOT
@@ -34,36 +36,36 @@
 #endif
 
 #include <ArduinoJson.h>
+//---------------------------------------------------------------------------------------------------------------------------------------
 const unsigned int CHARACTER_LIMIT= 10000; // Limit of characters in HTTP response
 //---------------------------------------------------------------------------------------------------------------------------------------
 // WiFi Connection
-const char* ssid = "SSID";          // replace these with the name and password for your local wi-fi network
-const char* password = "PASSWORD";
+const char* ssid = "Major Crimes";          // replace these with the name and password for your local wi-fi network
+const char* password = "The Bunk";
 //---------------------------------------------------------------------------------------------------------------------------------------
 // API
-String host = "api.carbonintensity.org.uk";
-String url = "/intensity/date";            
-
+String host = "raw.githubusercontent.com";
+String url = "/Edinburgh-College-of-Art/Connected-Things/master/example-code/json/example-data.json";
 //---------------------------------------------------------------------------------------------------------------------------------------
 // WiFi Client
 WiFiSSLClient sslClient;  // HTTPS
 WiFiClient client;        // HTTP
 char httpResponse[CHARACTER_LIMIT]; // If you are getting responses larger that 10kB, use another request.
 //---------------------------------------------------------------------------------------------------------------------------------------
-// Timer 
+// Timer
 unsigned long updateTimer = 300000;
 //---------------------------------------------------------------------------------------------------------------------------------------
 void setup()
 {
-  Serial.begin(9600); 
+  Serial.begin(9600);
   while (!Serial) {} // This line is blocking, remove it when you are finished
-  
+
   connectToWifi(ssid, password);
-    
-  JsonObject json = makeAPIcall(host, url, sslClient, 443);
-  
-  String someData = json["data"][0]["intensity"]["forecast"]; // This will change depending on the API of course
- 
+
+  JsonObject json = makeAPIcall(host, url, client, 443);
+
+  String someData = json["say"]["hello"][2]["json"]; // This will change depending on the API of course
+
   Serial.println("\n\nSome Data: ");Serial.println(someData);
   Serial.println("Now Get to Work!");
   haltFirmware();
