3b5cd371974003887a76295dad47550b5e2e99b2
mhamilt <matt.hamilton@ed.ac.uk>
2020-03-09 12:53:45 +0000

    refactor uid functions

--- example-code/nfc-rfid/pn532/PN532_GetID/uuid_funcs.ino
+++ example-code/nfc-rfid/pn532/PN532_GetID/uuid_funcs.ino
@@ -1,9 +1,9 @@
-bool isUuidInList(uint32_t testUuid)
+bool isUuidInList(uint32_t testUuid, uint32_t* uuidList, uint8_t listLength)
 {
   bool inList = false;
-  for (int i = 0; i < numUuids; i++)
+  for (int i = 0; i < listLength; i++)
   {
-    if(idList[i] == testUuid)
+    if (uuidList[i] == testUuid)
     {
       inList = true;
       break;
@@ -11,10 +11,13 @@ bool isUuidInList(uint32_t testUuid)
   }
   return inList;
 }
-
+//==============================================================================
 void addToList(uint32_t newUuid)
 {
+  Serial.print("adding to list uid: ");
+  Serial.print("0x");
+  Serial.print(newUuid, HEX);
   idList[idIndex] = newUuid;
   idIndex++;
-  idIndex%=numUuids;
+  idIndex %= numUuids;
 }
